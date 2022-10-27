184710d6b2f303e7202f3aefb442f2f435591617
Brandon de Jesús Lara Ruiz <74059945+BrandonJLR-UTAGS@users.noreply.github.com>
2022-10-27 12:47:16 -0500

    Validacion de pares y Cuenta de movimientos

--- main.js
+++ main.js
@@ -5,48 +5,66 @@ let kard2 = null;
 let PrimerCarta = null;
 let SegundaCarta = null;
 let CartaSelecionada = null;
+let movimientos = null;
+let aciertos = null;
+//Elementos del HTML
+let ImprimirMovimientos = document.getElementById('movimientos');
+let ImprimirAciertos = document.getElementById('aciertos');
 
 //array de cartas
 let kards = [
   "carta1",
-  "carta1",
-  "carta2",
   "carta2",
   "carta3",
-  "carta3",
-  "carta4",
   "carta4",
   "carta5",
-  "carta5",
-  "carta6",
   "carta6",
   "carta7",
-  "carta7",
-  "carta8",
   "carta8",
   "carta9",
-  "carta9",
-  "carta10",
   "carta10",
   "carta11",
-  "carta11",
-  "carta12",
   "carta12",
   "carta13",
-  "carta13",
-  "carta14",
   "carta14",
   "carta15",
-  "carta15",
   "carta16",
-  "carta16",
+  
 ];
 
-//Turbo generador de nume ros al azar
-kards = kards.sort(() => {
+  //Turbo generador de numeros al azar
+  //Mescla todas las cartas
+
+  let KardsMezcladas =[];
+  KardsMezcladas = kards.sort(() => {
+    return Math.random() - 0.5;
+  });
+
+//seleciona solo las primeras 8 cartas de la mezcla
+  let KardsJuego = [];
+
+  for(let index = 0; index < 8; index++){
+    const item = KardsMezcladas[index];
+    //coloca los elementos en un nuevo array 'KardsJuego'
+    KardsJuego.push(`${item}`);
+
+  }
+  console.log(KardsJuego);
+
+//Crea una copia de las cartas selecionadas
+let CopiaKardsJuego=KardsJuego;
+console.log(CopiaKardsJuego);
+//Crea un mazo de las cartas selecionadas y la copia de estas
+let UnionKardCopiadas = CopiaKardsJuego.concat(KardsJuego);
+console.log(UnionKardCopiadas)
+
+//Turbo generador de numeros al azar
+//Mezcla el mazo de cartas
+kards = UnionKardCopiadas.sort(() => {
   return Math.random() - 0.5;
 });
-console.log(kards);
+
+console.log(kards)
 
 // //Muestra tarjetas restantes
 // function BloquearTarjetas(kards){
@@ -79,29 +97,60 @@ function MostrarCarta(id) {
     //apagar carta
     kard2.disabled = true;
 
-    //
-    
+    //Incrementar movimientos echos por el jugador
+    movimientos++;    
+    ImprimirMovimientos.innerHTML = `Movimientos: ${movimientos}`;
 
-  } else {
-    //gira las cartas //gira las cartas segun el ID de la carta
-    CartaSelecionada = document.getElementById(id);
-    console.log(CartaSelecionada);
-    Carta = kards[id];
-    CartaSelecionada.innerHTML = `<img src="./images/${Carta}.jpg" alt="">`;
-    //Se apaga el boton
-    CartaSelecionada.disabled = true;
-    
-    
-  }
-  //si se selecciona una tercera carta se voltean las cartas antes selecionadas
-  if(cartasDestapadas >= 3){
-    
-    kard1.innerHTML='';
-    kard2.innerHTML = '';
-    CartaSelecionada.innerHTML='';
-    kard1.disabled=false;
-    kard2.disabled=false;
-    CartaSelecionada.disabled=false;
-    cartasDestapadas=0;
-  }
+     if(PrimerCarta == SegundaCarta){
+      //Tapar targetas
+      cartasDestapadas = 0;
+      //Incrementa los aciertos
+      aciertos++;
+      ImprimirAciertos.innerHTML = `Aciertos: ${aciertos}`;
+
+      if(aciertos == 8){
+        ImprimirAciertos = `Aciertos: ${aciertos} WoW`;
+        ImprimirMovimientos = `Movimientos ${movimientos}`;
+      }
+
+    } 
+    else{
+      //mostrar valores y tapar de nuevo
+      setTimeout(()=>{
+        kard1.innerHTML='';
+          kard2.innerHTML = '';
+          //reactiva las cartas
+          kard1.disabled=false;
+          kard2.disabled=false;
+          //Reinicia constador de carta destapadas
+          cartasDestapadas=0;
+      },1000);
+    }
+  } 
+
+
+                    //else {
+                      // //gira las cartas //gira las cartas segun el ID de la carta
+                      // CartaSelecionada = document.getElementById(id);
+                      // console.log(CartaSelecionada);
+                      // Carta = kards[id];
+                      // CartaSelecionada.innerHTML = `<img src="./images/${Carta}.jpg" alt="">`;
+                      // //Se apaga el boton
+                      // CartaSelecionada.disabled = true;
+
+                      
+                      
+                      
+                    //}
+                    //si se selecciona una tercera carta se voltean las cartas antes selecionadas
+                    // if(cartasDestapadas >= 3){
+                      
+                    //   kard1.innerHTML='';
+                    //   kard2.innerHTML = '';
+                    //   CartaSelecionada.innerHTML='';
+                    //   kard1.disabled=false;
+                    //   kard2.disabled=false;
+                    //   CartaSelecionada.disabled=false;
+                    //   cartasDestapadas=0;
+                    // }
 }
